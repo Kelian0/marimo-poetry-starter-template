d983f59417e542e2c2c6dcab8fb4d18c9d87b77e
Kélian PONS <kelian.pons@centrale.centralelille.fr>
2025-10-03 10:52:35 +0200

    First testing with Marimo

--- src/app.py
+++ src/app.py
@@ -1,5 +1,6 @@
 import marimo
 
+__generated_with = "0.16.5"
 app = marimo.App(width="medium")
 
 
@@ -11,12 +12,38 @@ def _():
     return (mo,)
 
 
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""This is a markdown""")
+    return
+
+
 @app.cell
 def test_cell():
+
+
     from utils import add
 
     assert add(1, 2) == 3
     assert 2 == 2
+    return
+
+
+@app.cell
+def _(mo):
+    mo.md("# This is a print")
+
+    print("test")
+    return
+
+
+@app.cell
+def _():
+    return
+
+
+@app.cell
+def _():
     return
 
 
